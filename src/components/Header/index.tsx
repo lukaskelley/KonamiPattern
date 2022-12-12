4b9ca7e06f4587dbe449f18bfb3713492be45e2f
lukaskelley <lukaskelley412@gmail.com>
2022-12-13 03:32:19 +0900

    completed the konami pattern

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        background: "rgba(0,0,0,0.2)",
+        textAlign: "center",
+        padding: "10px",
+        marginBottom: "1%",
+      }}>
+      <h2>Sweet kittens</h2>
+    </header>
+  );
+}
